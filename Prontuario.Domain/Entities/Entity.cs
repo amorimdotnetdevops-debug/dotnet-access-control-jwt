ec135a040b92eeb2afe670d4ae60e9627eb9470f
Claudio Santana <claudio_desenv@hotmail.com>
2025-10-22 22:04:21 -0300

    Enhance domain model with Entity and DomainException
    Refactored the `Usuario` class to inherit from the new abstract `Entity` class, promoting code reuse and consistency across domain entities. Introduced `DomainException` to handle domain-specific errors more expressively. Added `Entity` class with common properties like `Id`, `DataCriacao`, and `DataAtualizacao` to standardize entity creation and tracking.

---
+++ Prontuario.Domain/Entities/Entity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prontuario.Domain.Entities
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; protected set; }
+        public DateTime DataCriacao { get; protected set; }
+        public DateTime? DataAtualizacao { get; protected set; }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+            DataCriacao = DateTime.UtcNow;
+        }
+    }
+}
--- Prontuario.Domain/Entities/Usuario.cs
+++ Prontuario.Domain/Entities/Usuario.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Prontuario.Domain.Entities
 {
-    public class Usuario
+    public class Usuario : Entity
     {
     }
 }
